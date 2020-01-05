99bcf19041ae784e464f4f3d52a052038984a662
Dylan Mikus <dmikus@hioscar.com>
2020-01-05 16:19:48 -0500

    Make titanic `build_pipeline` a sub-module

--- titanic/titanic/build_pipeline/__init__.py
+++ titanic/titanic/build_pipeline/__init__.py
@@ -1,17 +1,23 @@
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import mean_absolute_error
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 
-from . import clean_data
+from . import preprocessors
+from . import models
 
-def build_pipeline():
+def build_default_pipeline():
+    return build_pipeline(
+        preprocessors.build_preprocessor,
+        models.define_model
+    )
+
+def build_pipeline(preprocessor_fn, model_fn):
     # Bundle preprocessing and modeling code in a pipeline
-    my_pipeline = Pipeline(steps=[('preprocessor', build_preprocessor()),
-                                  ('model', define_model())
+    my_pipeline = Pipeline(steps=[('preprocessor', preprocessor_fn()),
+                                  ('model', model_fn())
                                  ])
     return my_pipeline
 
@@ -39,27 +45,3 @@ def make_predictions(pipeline, df):
         index=df.index,
         columns=['Survived']
     )
-
-def define_model():
-    return LogisticRegression()
-
-def build_preprocessor():
-    numerical_cols = clean_data.numerical_features()
-    categorical_cols = clean_data.categorical_features()
-
-    # Preprocessing for numerical data
-    numerical_transformer = SimpleImputer(strategy='constant')
-
-    # Preprocessing for categorical data
-    categorical_transformer = Pipeline(steps=[
-        ('imputer', SimpleImputer(strategy='most_frequent')),
-        ('onehot', OneHotEncoder(handle_unknown='ignore'))
-    ])
-
-    # Bundle preprocessing for numerical and categorical data
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ('num', numerical_transformer, numerical_cols),
-            ('cat', categorical_transformer, categorical_cols)
-        ])
-    return preprocessor
---
+++ titanic/titanic/build_pipeline/models.py
@@ -0,0 +1,4 @@
+from sklearn.linear_model import LogisticRegression
+
+def define_model():
+    return LogisticRegression()
