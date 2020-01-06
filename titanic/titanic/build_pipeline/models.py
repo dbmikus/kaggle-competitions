fbc6ecc7fd43a8d3d78d9b90d0862f963927482d
Dylan Mikus <dmikus@hioscar.com>
2020-01-05 22:44:39 -0500

    Try other titanic models without success

--- titanic/titanic/build_pipeline/models.py
+++ titanic/titanic/build_pipeline/models.py
@@ -1,5 +1,13 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.neural_network import MLPClassifier
 
 
 def define_model():
     return LogisticRegression(max_iter=200)
+
+def define_svm_model():
+    return svm.SVC()
+
+def define_nn_model():
+    return MLPClassifier()
